4f73983f023d57d2241a9b33eb23aed3d8bd6c55
AliAnilKocak <alianilkocak@gmail.com>
2021-09-29 14:55:56 +0300

    create user model

---
+++ src/main/java/com/nibblebytes/elasticsearch/entity/User.java
@@ -0,0 +1,32 @@
+package com.nibblebytes.elasticsearch.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Document(indexName = "kisiler")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+    @Field(name = "Name", type = FieldType.Text)
+    private String name;
+    @Field(name = "surname", type = FieldType.Text)
+    private String surName;
+    @Field(name = "Address", type = FieldType.Text)
+    private String address;
+    @Field(name = "Birth Date", type = FieldType.Date)
+    private Date birthDate;
+
+}
